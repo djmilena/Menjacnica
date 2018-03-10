598d3dd96129404e3a00d31b95a4f96b354e8469
djordjevic6016 <mimi.djordjevic97@gmail.com>
2018-03-10 10:11:57 +0100

    Generisane metode toString, equals i hashCode

--- Menjacnica/src/valuta/Valuta.java
+++ Menjacnica/src/valuta/Valuta.java
@@ -46,6 +46,59 @@ public class Valuta {
 	public void setProdajniKurs(double prodajniKurs) {
 		this.prodajniKurs = prodajniKurs;
 	}
+	@Override
+	public String toString() {
+		return "Valuta naziv=" + naziv + ", skrNaziv=" + skrNaziv + ", datum=" + datum + ", kupovniKurs=" + kupovniKurs
+				+ ", srednjiKurs=" + srednjiKurs + ", prodajniKurs=" + prodajniKurs;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		temp = Double.doubleToLongBits(prodajniKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skrNaziv == null) ? 0 : skrNaziv.hashCode());
+		temp = Double.doubleToLongBits(srednjiKurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Valuta))
+			return false;
+		Valuta other = (Valuta) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (Double.doubleToLongBits(kupovniKurs) != Double.doubleToLongBits(other.kupovniKurs))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (Double.doubleToLongBits(prodajniKurs) != Double.doubleToLongBits(other.prodajniKurs))
+			return false;
+		if (skrNaziv == null) {
+			if (other.skrNaziv != null)
+				return false;
+		} else if (!skrNaziv.equals(other.skrNaziv))
+			return false;
+		if (Double.doubleToLongBits(srednjiKurs) != Double.doubleToLongBits(other.srednjiKurs))
+			return false;
+		return true;
+	}
 	
 	
 	
