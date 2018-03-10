e57790103f3d58d3eb48d6cc4bb1baa2ba16a56c
djordjevic97 <mimi.djordjevic97@gmail.com>
2018-03-10 11:38:37 +0100

    Implementiran izmenjen interfejs Menjacnica
    uveden privatni atribut listaValuta u klasi Menjacnica1
    Menjacnica (interfejs) sada sa novim argumentima metoda

--- Menjacnica/src/interfejs/Menjacnica.java
+++ Menjacnica/src/interfejs/Menjacnica.java
@@ -1,13 +1,12 @@
 package interfejs;
 
-import java.util.GregorianCalendar;
 
 import valuta.Valuta;
 
 public interface Menjacnica {
 
-	public void dodajKurs(String skrNaziv, GregorianCalendar datum, double kupovni, double prodajni);
-	public void obrisiKurs(String skrNaziv, GregorianCalendar datum);
-	public Valuta nadjiKurs(String skrNaziv, GregorianCalendar datum);
+	public void dodajKurs(Valuta v);
+	public void obrisiKurs(Valuta v);
+	public Valuta nadjiKurs(Valuta v);
 	
 }
