c5fa5fb3826b7737b0dd4c7535d779153439d994
djordjevic97 <mimi.djordjevic97@gmail.com>
2018-03-10 13:44:38 +0100

    Implementacija interfejsa Menjacnica u master grani

--- Menjacnica/src/klase/Menjacnica1.java
+++ Menjacnica/src/klase/Menjacnica1.java
@@ -1,27 +1,33 @@
 package klase;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs.Menjacnica;
 import valuta.Valuta;
 
 public class Menjacnica1 implements Menjacnica {
-
+	
+	private LinkedList<Valuta> listaValuta = new LinkedList<Valuta>();
+	
 	@Override
-	public void dodajKurs(String skrNaziv, GregorianCalendar datum, double kupovni, double prodajni) {
+	public void dodajKurs(Valuta v) {
 		// TODO Auto-generated method stub
-
+		 listaValuta.addLast(v);
 	}
 
 	@Override
-	public void obrisiKurs(String skrNaziv, GregorianCalendar datum) {
+	public void obrisiKurs(Valuta v) {
 		// TODO Auto-generated method stub
-
+		if(listaValuta.contains(v))
+			listaValuta.remove(v);
 	}
 
 	@Override
-	public Valuta nadjiKurs(String skrNaziv, GregorianCalendar datum) {
+	public Valuta nadjiKurs(Valuta v) {
 		// TODO Auto-generated method stub
+		if(listaValuta.contains(v))
+			return v;
 		return null;
 	}
 
