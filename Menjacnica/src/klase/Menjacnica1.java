968801f3549fabad0893fc335f5dc9e7c39bb33a
djordjevic97 <mimi.djordjevic97@gmail.com>
2018-03-10 10:59:28 +0100

    Menjacnica1 - prazna implementacija interfejsa Menjacnica

---
+++ Menjacnica/src/klase/Menjacnica1.java
@@ -0,0 +1,28 @@
+package klase;
+
+import java.util.GregorianCalendar;
+
+import interfejs.Menjacnica;
+import valuta.Valuta;
+
+public class Menjacnica1 implements Menjacnica {
+
+	@Override
+	public void dodajKurs(String skrNaziv, GregorianCalendar datum, double kupovni, double prodajni) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKurs(String skrNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Valuta nadjiKurs(String skrNaziv, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
